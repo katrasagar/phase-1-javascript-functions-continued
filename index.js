2aef3f8e9b48eefdb3c5ee5ffc5262e73d432016
Your Name <aqatra6@gmail.com>
2024-05-16 13:08:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+function saturdayFun() {
+    console.log('This Saturday, I want to roller-skate!');
+  }
+  saturdayFun();
+  
+  function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun());
+  
+  function mondayWork() {
+    console.log("This Monday, I will ... .");
+  }
+  mondayWork();
+  
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  console.log(mondayWork());
+  
+  function wrapAdjective(highlight = '*') {
+    return function (adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
